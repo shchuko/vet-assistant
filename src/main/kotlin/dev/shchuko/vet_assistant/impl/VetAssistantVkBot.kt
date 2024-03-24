bd59108f7cc6707484d3cd7dfb25820b3b62818f
Vladislav.Yaroshchuk <vladislav.yaroshchuk@jetbrains.com>
2024-03-24 22:15:02 +0400

    Add VK bot implementation

---
+++ src/main/kotlin/dev/shchuko/vet_assistant/impl/VetAssistantVkBot.kt
@@ -0,0 +1,34 @@
+package dev.shchuko.vet_assistant.impl
+
+import com.petersamokhin.vksdk.core.client.VkApiClient
+import com.petersamokhin.vksdk.core.model.VkSettings
+import com.petersamokhin.vksdk.core.model.event.MessageNew
+import com.petersamokhin.vksdk.http.VkOkHttpClient
+import kotlinx.coroutines.*
+
+
+class VetAssistantVkBot(groupId: Int, apiKey: String) : VetAssistantBot() {
+
+    @OptIn(ExperimentalCoroutinesApi::class)
+    private val vkHttpClient = VkOkHttpClient()
+
+    @OptIn(ExperimentalCoroutinesApi::class)
+    val client = VkApiClient(groupId, apiKey, VkApiClient.Type.Community, VkSettings(vkHttpClient)).also { client ->
+        client.onMessage { handleMessage(it) }
+    }
+
+    private val cs = CoroutineScope(Dispatchers.Default)
+
+
+    private fun handleMessage(event: MessageNew): Job = cs.launch {
+        val result = handleSearchMedicineRequest(event.message.text) ?: return@launch
+        client.sendMessage {
+            peerId = event.message.peerId
+            message = result
+        }.execute()
+    }
+
+    override suspend fun startPolling() {
+        client.startLongPolling()
+    }
+}
